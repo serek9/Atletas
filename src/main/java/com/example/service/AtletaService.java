bc5d234ba481dc258b51d91d435e0239bc5de124
sergiodiaz <sergiodiaz808@gmail.com>
2016-12-12 14:03:21 +0100

    service

--- src/main/java/com/example/service/AtletaService.java
+++ src/main/java/com/example/service/AtletaService.java
@@ -20,13 +20,13 @@ public class AtletaService {
 
         Atleta atleta1 = new Atleta("Alberto", "Comuñas", "Suiza", LocalDate.of(1990,12,12));
         atletaRepository.save(atleta1);
-        Atleta atleta2 = new Atleta("Sergio", "Díaz", "Mundo", LocalDate.of(1990,12,12));
+        Atleta atleta2 = new Atleta("Sergio", "Díaz", "Mundo", LocalDate.of(1996,8,8));
         atletaRepository.save(atleta2);
-        Atleta atleta3 = new Atleta("Nando", "Aranda", "U.S.", LocalDate.of(1990,12,12));
+        Atleta atleta3 = new Atleta("Nando", "Aranda", "U.S.", LocalDate.of(1991,12,12));
         atletaRepository.save(atleta3);
-        Atleta atleta4 = new Atleta("Larry", "Andre", "Peru", LocalDate.of(1990,12,12));
+        Atleta atleta4 = new Atleta("Larry", "Andre", "Peru", LocalDate.of(1992,12,12));
         atletaRepository.save(atleta4);
-        Atleta atleta5 = new Atleta("Victor", "Amador", "Francia", LocalDate.of(1990,12,12));
+        Atleta atleta5 = new Atleta("Victor", "Amador", "Francia", LocalDate.of(1993,12,12));
         atletaRepository.save(atleta5);
 
     }

5050cd7ae831ef05a6cc59aae06d296ce3b451a4
sergiodiaz <sergiodiaz808@gmail.com>
2016-12-13 12:50:33 +0100

    Queries controller.

--- src/main/java/com/example/controller/AtletaController.java
+++ src/main/java/com/example/controller/AtletaController.java
@@ -2,12 +2,17 @@ package com.example.controller;
 
 
 import com.example.domain.Atleta;
+import com.example.domain.Medalla;
 import com.example.repository.AtletaRepository;
+import com.example.repository.MedallaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -16,6 +21,8 @@ public class AtletaController {
 
     @Autowired
     private AtletaRepository atletaRepository;
+    @Autowired
+    private MedallaRepository medallaRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -38,8 +45,31 @@ public class AtletaController {
     }
 
     //ATLETAS K HAYAN NACIDO ANTERIOR A FECHA DETERMINADA
+    @GetMapping("/byNacimientoAntes/{nacimiento}")
+    public List<Atleta> findByNacimientoAntes(@PathVariable String nacimiento){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate fecha = LocalDate.parse(nacimiento, formatter);
+        return atletaRepository
+                .findAll()
+                .parallelStream()
+                .filter(atleta -> atleta.getFechan().isBefore(fecha))
+                .collect(Collectors.toList());
+    }
 
     //ATLETAS AGRUPADOS POR NACIONALIDAD EN UN MAP(LIST ATLETA)
-
+    @GetMapping("/byNacionalidadMap")
+    public Map<String, List<Atleta>> findByNacionalidadMap(){
+        return atletaRepository
+                .findAll()
+                .parallelStream()
+                .collect(Collectors.groupingBy(Atleta::getNacionalidad));
+    }
     //ATLETAS AGRUPADOS POR MEDALLLA EN UN MAP(LIST ATLETA)
+    @GetMapping("/byTipoMedalla")
+    public Map<enum Enumeracion{Oro, Plata, Bronce}, List<Medalla>> findByTipoMedalla(){
+        return medallaRepository
+                .findAll()
+                .parallelStream()
+                .collect(Collectors.groupingBy(Medalla::))
+    }
 }
